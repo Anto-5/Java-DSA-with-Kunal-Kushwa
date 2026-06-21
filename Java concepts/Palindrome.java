4785174a75959b89dcc5de92b0427c6b62a2a52c
Anto-5 <anmolbisht002@gmail.com>
2026-06-21 21:36:09 +0530

    Same word in reverse

---
+++ Java concepts/Palindrome.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = scn.nextLine();
+
+        String rev = "";
+
+        for(int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if(str.equals(rev)) {
+            System.out.println("It is in fact, without a shadow of doubt, a Palindrome!");
+        }
+        else {
+            System.out.println("It is not a Palindrome, oh shucks, what a bummer!");
+        }
+
+        scn.close();
+    
+    }   
+}
