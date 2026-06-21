83afab4d7425806bf553fa0b7f5ceb3b791d2324
Anto-5 <anmolbisht002@gmail.com>
2026-06-21 21:00:33 +0530

    Greeting your name

---
+++ Java concepts/Greeting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Greeting
+{
+       public static void main(String args[])
+    {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your name fren :) : ");
+        String name = scn.nextLine();
+        System.out.println("Hello " + name + " nice to meet you!" + " You are kind of short");
+        System.out.println("Oh sorry, did not mean to say that out loud ;)");
+    }
+}
